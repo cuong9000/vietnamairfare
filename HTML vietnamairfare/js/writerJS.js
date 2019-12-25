d751a1caa6c443af73c55bfecb217d99de372397
cuong9000 <cuong13071988@gmail.com>
2019-12-26 00:42:09 +0700

    update search

--- HTML vietnamairfare/js/writerJS.js
+++ HTML vietnamairfare/js/writerJS.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function(){
-	
+	$('.top_number_adult').click(function(){
+		$(this).parent().toggleClass("active");
+	  });
+	$('.destination-popup__close').click(function(){
+		$(".top_input_search").removeClass('active');
+	  });
 	
 });
