4bbad35e368b0aeb557fc8577252f7920db9f3c7
cuongNM <cuongnm@runsystem.net>
2019-12-26 12:07:25 +0700

    update

--- HTML vietnamairfare/js/writerJS.js
+++ HTML vietnamairfare/js/writerJS.js
@@ -7,4 +7,9 @@ $(document).ready(function(){
 		$(".top_input_search").removeClass('active');
 	  });
 	
+	$('.row3_search a').click(function(){
+		$(".line_search_flight").removeClass('active');
+		$(this).parent().parent().parent().toggleClass("active");
+	  });
+	
 });
